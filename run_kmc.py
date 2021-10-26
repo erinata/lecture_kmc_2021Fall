4edd2f334699670ce3ab9987b70106665120944f
Chungsang Tom Lam <erinata@gmail.com>
2021-10-27 04:40:22 +0800

    run kmeans

--- run_kmc.py
+++ run_kmc.py
@@ -5,5 +5,18 @@ import matplotlib.pyplot as pyplot
 
 data = pandas.read_csv("dataset.csv")
 
-print(data)
+# print(data)
 
+data = data.values
+
+# print(data)
+
+pyplot.scatter(data[:,0], data[:,1])
+pyplot.savefig("scatterplot.png")
+
+
+machine = KMeans(n_clusters=4)
+machine.fit(data)
+results = machine.predict(data)
+
+print(results)
