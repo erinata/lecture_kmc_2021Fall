f7936ba222654cdc51b184d60d5d602cb695a523
Chungsang Tom Lam <erinata@gmail.com>
2021-11-03 03:46:11 +0800

    run kmc

--- run_kmc.py
+++ run_kmc.py
@@ -2,7 +2,6 @@ import pandas
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as pyplot
 
-
 data = pandas.read_csv("dataset.csv")
 
 # print(data)
@@ -15,8 +14,27 @@ pyplot.scatter(data[:,0], data[:,1])
 pyplot.savefig("scatterplot.png")
 
 
-machine = KMeans(n_clusters=4)
-machine.fit(data)
-results = machine.predict(data)
+def run_kmeans(n, data):
+	machine = KMeans(n_clusters=n)
+	machine.fit(data)
+	results = machine.predict(data)
+	centroids = machine.cluster_centers_
+	ssd = machine.inertia_
+	print(ssd)
+	pyplot.scatter(data[:,0], data[:,1], c=results)
+	pyplot.scatter(centroids[:,0], centroids[:,1], c='red', marker="*", s=200)
+	pyplot.savefig("scatterplot_color_" + str(n) + ".png")
+	pyplot.close()
 
-print(results)
+
+run_kmeans(1, data)
+run_kmeans(2, data)
+run_kmeans(3, data)
+run_kmeans(4, data)
+run_kmeans(5, data)
+run_kmeans(6, data)
+run_kmeans(7, data)
+
+
+
+
