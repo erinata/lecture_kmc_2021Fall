711fb649724b5253559d118c69f429f7f43b157b
Chungsang Tom Lam <erinata@gmail.com>
2021-11-03 04:09:32 +0800

    condense code

--- run_kmc.py
+++ run_kmc.py
@@ -26,22 +26,24 @@ def run_kmeans(n, data):
 	pyplot.close()
 	return ssd
 
-result = []
-for i in range(7):
-	ssd = run_kmeans(i+1, data)	
-	result.append(ssd)
+# result = []
+# for i in range(7):
+# 	ssd = run_kmeans(i+1, data)	
+# 	result.append(ssd)
 
+result = [ run_kmeans(i+1, data) for i in range(7)]
 print(result)
 
 pyplot.plot(range(7), result)
 pyplot.savefig("ssd.png")
 pyplot.close()
 
-result_diff = []
-for i,x  in enumerate(result):
-	diff = result[i-1] - x
-	result_diff.append(diff)
+# result_diff = []
+# for i,x  in enumerate(result):
+# 	diff = result[i-1] - x
+# 	result_diff.append(diff)
 
+result_diff = [ result[i-1] - x for i,x  in enumerate(result)][1:]
 print(result_diff)
 
 
